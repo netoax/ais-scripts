178ffee2f97475e5a809d8c17f0a12f7ae74172a
J Neto <j.alexandreeneto@gmail.com>
2017-06-17 10:11:36 +0000

    running tcpdump in background

--- tanenbaum/up_sub.sh
+++ tanenbaum/up_sub.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-sudo tcpdump -i eth0 host 192.168.1.103 and port 1883 -w log_sub &
+nohup sudo tcpdump -i wlan0 port 1883 -w log_sub &>/dev/null & sleep 2
 
-mosquitto_sub -h 192.168.1.102 -t sensor/temperatura/
+echo "Ativou tcp dump -> verificar processos, esperar 5 segundos"
 
-echo "Passou sub"
+sleep 5
+
+mosquitto_sub -h 192.168.0.104 -t sensor/temperatura/
+
+echo "Passou pelo sub -> algo está errado"
 
 #tcpdump_num="$(ps aux | grep tcpdump | sed 1'!d' | cut -d" " -f7)"
 #sudo kill $tcpdump_num
