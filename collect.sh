3d7f5db3c791e566eaf4e0fcc4aae6c943c04d4f
silvanoneto <silvano.neto@upe.br>
2017-06-22 18:19:06 -0300

    add log

--- collect.sh
+++ collect.sh
@@ -4,16 +4,34 @@ arq="arq.txt"
 
 len="$(wc $arq | cut -d" " -f3)"
 
+tempo_inicial=""
+esp_ack=""
+
 for ((i=1; i<$len; i++))
 do
 	line="$(sed $i'!d' $arq)"
-	
-	
 	param="$(cut -d" " -f7 $line)"
+
 	if $param -eq '[P.]'
 	then
-		echo ${line}
 		esp_ack="$(cut -d" " -f9 $line | cut -d: -f2)"
+
+		for ((j=i+1; j<$len; j++))
+		do
+			line2="(sed $j'!d' $arq)"
+			param2="$(cut -d" " -f7 $line2)"
+
+			if $param2 -eq '[P.]'
+			then
+				act_ack="$(cut -d" " -f11 $line2)"
+
+				if $esp_ack -eq $act_ack
+				then
+					echo "OK"
+					break
+				fi
+			fi
+		done
 	fi
 done
 
